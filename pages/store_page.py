52b151b57a1b24130ddcf27b318c7e93a9be2ca0
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-10-16 18:10:31 +0200

    Added Store_page.

--- pages/store_page.py
+++ pages/store_page.py
@@ -1,4 +1,10 @@
+from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 
 
 class StorePage(BasePage):
+    _products_list = (By.CSS_SELECTOR, "ul[class='products columns-4']")
+
+    @property
+    def loaded(self):
+        return self.is_element_displayed(*self._products_list)
