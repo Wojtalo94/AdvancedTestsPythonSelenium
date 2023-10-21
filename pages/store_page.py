f0382867e28660028819897c7d096e9e14586e78
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-10-21 11:55:20 +0200

    Added find_item_by_name function to add item do cart.

--- pages/store_page.py
+++ pages/store_page.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
+from helpers.helpers import find_item_by_name
 from pages.base_page import BasePage
+from pages.regions.item import Item
 
 
 class StorePage(BasePage):
@@ -9,3 +11,10 @@ class StorePage(BasePage):
     @property
     def loaded(self):
         return self.is_element_displayed(*self._products_list)
+
+    @property
+    def items(self):
+        return [Item(self, product) for product in self.find_elements(*self._product)]
+
+    def add_item_to_cart(self, item_name):
+        find_item_by_name(self.items, item_name).click_add_to_cart_button()
