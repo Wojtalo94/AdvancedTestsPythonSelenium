a5f60f235ae0c40810bde05590625340a4b8d4bd
Wojciech Sumara <67499762+Wojtalo94@users.noreply.github.com>
2023-10-29 18:47:47 +0100

    Adding verification of the data from the shopping cart before placing the order with asserts and clicking the "Go to payment" button.

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,11 +1,16 @@
 from selenium.webdriver.common.by import By
 from helpers.helpers import find_item_by_name
 from pages.base_page import BasePage
+from pages.regions.base_region import BaseRegion
 
 
 class CartPage(BasePage):
     _cart_title = (By.XPATH, "//h1[contains(text(), 'Koszyk')]")
     _product_in_the_cart = (By.CSS_SELECTOR, "tr[class*='cart_item']")
+    _delivery_fee = (By.CSS_SELECTOR, "td[data-title='Wysyłka'] span[class*='Price-amount']")
+    _vat = (By.CSS_SELECTOR, "td[data-title='VAT'] span[class*='Price-amount']")
+    _order_total_amount = (By.CSS_SELECTOR, "td[data-title='Łącznie'] span[class*='Price-amount']")
+    _checkout_button = (By.XPATH, "//a[@class='checkout-button button alt wc-forward']")
 
     @property
     def loaded(self):
@@ -14,6 +19,21 @@ class CartPage(BasePage):
     @property
     def items_in_the_cart(self):
         return [CartItem(self, product) for product in self.find_elements(*self._product_in_the_cart)]
+
+    @property
+    def delivery_fee(self):
+        fee = self.find_element(*self._delivery_fee).text
+        return fee[1:]
+
+    @property
+    def vat(self):
+        vat_fee = self.find_element(*self._vat).text
+        return vat_fee[1:]
+
+    @property
+    def order_total_amount(self):
+        total_amount = self.find_element(*self._order_total_amount).text
+        return total_amount[1:]
 
     def assert_item_data(self, item_name, item_unit_price, quantity="1", total_price=None):
         if total_price is None:
@@ -25,11 +45,14 @@ class CartPage(BasePage):
         assert item.quantity == quantity
         assert item.item_total_price == total_price
 
+    def click_checkout_button(self):
+        self.find_element(*self._checkout_button).click()
 
-class CartItem(BasePage):
+
+class CartItem(BaseRegion):
     _name = (By.CSS_SELECTOR, "td[class*='product-name']")
     _item_unit_price = (By.CSS_SELECTOR, "td[class*='product-price']")
-    _quantity = (By.CSS_SELECTOR, "td[class*='product-quantity'] input[aria-label='Ilość produktu']")
+    _quantity = (By.CSS_SELECTOR, "td[class*='product-quantity'] input[id*='quantity']")
     _item_total_price = (By.CSS_SELECTOR, "td[class*='product-subtotal'] ")
 
     @property
@@ -43,8 +66,8 @@ class CartItem(BasePage):
 
     @property
     def quantity(self):
-        item_quantity = self.find_element(*self._quantity).text
-        return item_quantity.get_atribute("value")
+        item_quantity = self.find_element(*self._quantity)
+        return item_quantity.get_attribute("value")
 
     @property
     def item_total_price(self):
--- tests/test_order_processing.py
+++ tests/test_order_processing.py
@@ -15,10 +15,13 @@ class TestOrderProcessing:
         store_page.add_item_to_cart("Belt")
 
         assert home_page.menu.amount == '65,00'
-
         home_page.menu.menu_pop_up().go_to_the_cart()
 
         cart_page = CartPage(self.driver).wait_for_page_to_load()
         cart_page.assert_item_data("Belt", "65,00")
 
-       # cart_page.assert_item_data("Belt", "65,00", "1", "65,00")
+        assert cart_page.delivery_fee == "5,00"
+        assert cart_page.vat == "16,10"
+        assert cart_page.order_total_amount == "86,10"
+
+        cart_page.click_checkout_button()
